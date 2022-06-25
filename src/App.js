51f8c73be8e05fd0477d6fef17d348e042c2ea35
mikoogla9 <rgmikoogla@gmail.com>
2022-06-25 21:51:02 +0200

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Content from "./components/Content/Content";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div className="App">empty project for now xD</div>;
+  return (
+    <>
+      <Navbar />
+      <Content />
+    </>
+  );
 }
 
 export default App;
